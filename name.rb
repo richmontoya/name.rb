270e54ec9f20ec0196360c5a39a43f52ccca5cdd
Rich Montoya <richmontoya1@gmail.com>
2013-12-12 11:45:23 -0800

    Create name.rb

---
+++ name.rb
@@ -0,0 +1,17 @@
+puts 'What is your first name?'
+first_name = gets.chomp
+first_name.capitalize!
+
+puts 'What is your last name?'
+last_name = gets.chomp
+last_name.capitalize!
+
+print 'What city are you from?'
+city = gets.chomp
+city.capitalize!
+
+print 'What state or province are you from?'
+state = gets.chomp
+state.upcase!
+
+puts 'Your name is ' + first_name + ' ' + last_name + ' and you are from '  + city + ' ' + state + '!'
